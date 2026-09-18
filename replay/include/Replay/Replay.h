7748ec61ad706e4802c225188131e455c5080285
LivingTheDagor <LivingTheDagor@gmail.com>
2026-09-17 22:43:18 -0400

    revered changed, was supposed to be on develop

--- replay/include/Replay/Replay.h
+++ replay/include/Replay/Replay.h
@@ -76,7 +76,7 @@ protected:
   class FileReplayData : public IReplayData {
   public:
     FullFileLoadCB reader;
-    std::vector<uint8_t> zstd_data{};
+    std::vector<uint8_t> zlib_data{};
     uint32_t ref_count = 0; // how many readers are using this data?
 
     explicit FileReplayData(const std::string &path) : reader(path) {}
@@ -171,8 +171,8 @@ protected:
   auto getData() { return Data.getData(this); }
   void load();
 
-  uint32_t zstd_offs = 0xFFFFFFFF;
-  uint32_t zstd_size = 0;
+  uint32_t zlib_offs = 0xFFFFFFFF;
+  uint32_t zlib_size = 0;
 
   friend FullDecompressReplayReader;
   friend CompressedReplayReader;
@@ -198,6 +198,7 @@ public:
   IReplayReader *getReplayReader() override;
   IReplayReader *getCompressedReplayReader() override;
   bool isValid() override { return is_valid; }
+  // IReplayReader * getStreamingReplayReader(uint32_t time_wait=10);
 };
 
 class ServerReplay final : public IReplay {
@@ -233,18 +234,18 @@ public:
 };
 
 template<bool doExist>
-struct _optionalZstd;
+struct _optionalZlib;
 
 template<>
-struct _optionalZstd<true> {
-  ZstdSaveCB writer;
+struct _optionalZlib<true> {
+  ZlibSaveCB writer;
 
-  _optionalZstd(IGenSave &save) : writer(save, 9) {}
+  _optionalZlib(IGenSave &save) : writer(save, 9) {}
 };
 
 template<>
-struct _optionalZstd<false> {
-  _optionalZstd(IGenSave &save) {}
+struct _optionalZlib<false> {
+  _optionalZlib(IGenSave &save) {}
 };
 
 template<typename T>
@@ -298,13 +299,13 @@ template<bool streamWrite>
 class ReplayWriter {
 
   DynamicMemGeneralSaveCB base_cb;
-  _optionalZstd<streamWrite> zstd_cb{base_cb};
+  _optionalZlib<streamWrite> zlib_cb{base_cb};
   uint32_t curr_time_ms = 0;
 
 
   auto &getWriter() {
     if constexpr (streamWrite) {
-      return zstd_cb.writer;
+      return zlib_cb.writer;
     } else {
       return base_cb;
     }
@@ -339,7 +340,7 @@ public:
   }
   ~ReplayWriter() {
     if constexpr (streamWrite)
-      zstd_cb.writer.finish();
+      zlib_cb.writer.finish();
   }
 
   void write(const void *data, size_t size, uint32_t time_ms, ReplayPacketType type) {
